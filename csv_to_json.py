0bfb790c85ae24fd3b12fbdddf5b04fd1ac2ebbf
Michael <whitn152@umn.edu>
2022-02-12 22:51:06 -0600

    Added CS & config file to load. Modified json py slightly

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,14 +1,19 @@
 import csv
 import json
+import uuid
 
 def make_json(csv_file_path, json_file_path):
     data = {}
     with open(csv_file_path, encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         with open(json_file_path, 'w', encoding='utf-8') as json_file:
-            for row in csv_reader:
+            json_file.write('[')
+            for index, row in enumerate(csv_reader):
                 new_row = rename_columns(delete_columns(row))
+                new_row['id'] = str(uuid.uuid4())
+                if index != 0: json_file.write(',')
                 json.dump(new_row,json_file, indent=4)
+            json_file.write(']')
 
 def delete_columns(row):
     remove_list = ['Parameter Code','Method Type','Method Code','Method Name','State Name','County Name']
