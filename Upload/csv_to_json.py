1916183888c5d370a2a79beebfed706ce5b46706
Andrew Bros <andrewbros@gmail.com>
2022-02-27 18:05:41 -0600

    Collapse 3 site fields into one
    Also combine longitude and latitude into a single geographic
    location.
    
    Allow input CSV file and output JSON filename to be specified as
    command line arguments.

--- Upload/csv_to_json.py
+++ Upload/csv_to_json.py
@@ -1,9 +1,25 @@
+#!/usr/bin/env python3
+"""
+Convert datasets from CSV format into JSON,
+dropping unneeded fields and combining others.
+"""
+
+import argparse
 import csv
 import json
 import uuid
 
+def get_args():
+    """Get command line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Convert datasets from CSV to JSON',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('csv', metavar='CSV file', help='Input CSV filename')
+    parser.add_argument('json', metavar='JSON file', help='Output JSON filename')
+    return parser.parse_args()
+
 def make_json(csv_file_path, json_file_path):
-    data = {}
+    """Read CSV, write JSON"""
     with open(csv_file_path, encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         with open(json_file_path, 'w', encoding='utf-8') as json_file:
@@ -11,43 +27,52 @@ def make_json(csv_file_path, json_file_path):
             for index, row in enumerate(csv_reader):
                 new_row = rename_columns(delete_columns(row))
                 new_row['id'] = str(uuid.uuid4())
-                if index != 0: json_file.write(',')
-                json.dump(new_row,json_file, indent=4)
+                if index != 0:
+                    json_file.write(',')
+                json.dump(new_row, json_file, indent=4)
             json_file.write(']')
 
 def delete_columns(row):
-    remove_list = ['Parameter Code','Method Type','Method Code','Method Name','State Name','County Name']
-    [row.pop(key) for key in remove_list]
-    return row
-        
+    """Drop unneeded fields"""
+    remove_list = [
+        'Parameter Code',
+        'Method Type',
+        'Method Code',
+        'Method Name',
+        'State Name',
+        'County Name'
+    ]
+    return {k: v for k, v in row.items() if k not in remove_list}
+
 def rename_columns(row):
+    """Map CVS fields to JSON labels"""
     rename_dict = {
-        'State Code':'stateCode',
-        'County Code':'countyCode',
-        'Site Num':'siteNum',
-        'POC':'poc',
-        'Latitude':'latitude',
-        'Longitude':'longitude',
-        'Datum':'datum',
-        'Parameter Name':'parameterName',
-        'Date Local':'dateLocal',
-        'Time Local':'timeLocal',
-        'Date GMT':'dateGMT',
-        'Time GMT':'timeGMT',
-        'Sample Measurement':'sampleMeasurement',
-        'Units of Measure':'unitOfMeasure',
-        'MDL':'mdl',
-        'Uncertainty':'uncertainty',
-        'Qualifier':'qualifier',
-        'Date of Last Change':'dateLastChange'
+        'POC': 'poc',
+        'Datum': 'datum',
+        'Parameter Name': 'parameterName',
+        'Date Local': 'dateLocal',
+        'Time Local': 'timeLocal',
+        'Date GMT': 'dateGMT',
+        'Time GMT': 'timeGMT',
+        'Sample Measurement': 'sampleMeasurement',
+        'Units of Measure': 'unitOfMeasure',
+        'MDL': 'mdl',
+        'Uncertainty': 'uncertainty',
+        'Qualifier': 'qualifier',
+        'Date of Last Change': 'dateLastChange'
     }
-    new_row = {rename_dict[old_column]: value for old_column, value in row.items()}
+    new_row = {rename_dict[k]: v for k, v in row.items() if k in rename_dict}
+    new_row['siteCode'] = f'{row["State Code"]}-{row["County Code"]}-{row["Site Num"]}'
+    new_row['location'] = {
+        'type': 'Point',
+        'coordinates': [row['Longitude'], row['Latitude']]
+    }
     return new_row
 
 def main():
-    csv_file_path = r"file_path.csv"
-    json_file_path = r"file_path.json"
-    make_json(csv_file_path, json_file_path)
+    """Convert CSV datasets to JSON"""
+    args = get_args()
+    make_json(args.csv, args.json)
 
 if __name__ == '__main__':
     main()
